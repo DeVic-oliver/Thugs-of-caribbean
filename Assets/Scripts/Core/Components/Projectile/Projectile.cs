b26a14aa9bfbf28998ff558bd58dfb8d3085db91
Victor Matheus <victormatheus183@gmail.com>
2023-02-11 22:38:27 -0300

    Projectile disable funcionality implemented

--- Assets/Scripts/Core/Components/Projectile/Projectile.cs
+++ Assets/Scripts/Core/Components/Projectile/Projectile.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.Core.Interfaces;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.Core.Components.Projectile
@@ -7,7 +8,9 @@ namespace Assets.Scripts.Core.Components.Projectile
     {
         [SerializeField] protected float _projectileSpeed;
         [SerializeField] protected int _damage;
-        
+
+        private float _timeToDisable = 1.5f;
+
         protected virtual void Update()
         {
             LaunchProjectile();
@@ -18,13 +21,29 @@ namespace Assets.Scripts.Core.Components.Projectile
             transform.Translate(Vector3.up * _projectileSpeed * Time.deltaTime);
         }
 
-        protected virtual void OnTriggerEnter(Collider other)
+        protected virtual void OnTriggerEnter2D(Collider2D other)
         {
             IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
             if (damageable != null)
             {
                 damageable.TakeDamage(_damage);
+                DisableMe();
             }
+        }
+        public void DisableMe()
+        {
+            _projectileSpeed = 0;
+            StartCoroutine("DisableInstance");
+        }
+        private IEnumerator DisableInstance()
+        {
+            float count = 0;
+            while(count < _timeToDisable)
+            {
+                count += Time.deltaTime;
+                yield return null;
+            }
+            gameObject.SetActive(false);
         }
     }
 }
