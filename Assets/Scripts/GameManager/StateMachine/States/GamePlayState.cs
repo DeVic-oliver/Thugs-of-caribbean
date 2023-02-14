4208a5590364acdc0c1c211f643738a016fb08e5
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-14 14:44:20 -0300

    Game manager statemachine implementation
    Baseic game states control;

---
+++ Assets/Scripts/GameManager/StateMachine/States/GamePlayState.cs
@@ -0,0 +1,54 @@
+﻿namespace Assets.Scripts.GameManager.StateMachine
+{
+    using Assets.Scripts.Core.Components.Counters;
+    using Assets.Scripts.Player;
+    using Devic.Scripts.Utils.StateMachine;
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+
+    public class GamePlayState : IConcreteState
+    {
+        private TimerCounter _gameTimer;
+        private PlayerHealth _playerHealth;
+        private PlayerInput _inputSystem;
+
+        private InputAction _pauseAction;
+
+        public GamePlayState(TimerCounter gameTimer, PlayerHealth playerHealth, PlayerInput inputSystem)
+        {
+            _gameTimer = gameTimer;
+            _playerHealth = playerHealth;
+            _inputSystem = inputSystem;
+            _pauseAction = _inputSystem.actions.FindAction("Pause");
+        }
+        public void OnStateEnter(StateMachine stateMachine)
+        {
+            ResumeGame();
+        }
+        private void ResumeGame()
+        {
+            if(Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+        }
+
+        public void OnUpdateState(StateMachine stateMachine)
+        {
+            ChangeToPauseStateWhenPauseActionIsPerformed(stateMachine);
+            ChangeToGameOverWhenPlayerDiesOrTimeEnds(stateMachine);
+        }
+        private void ChangeToPauseStateWhenPauseActionIsPerformed(StateMachine stateMachine)
+        {
+            _pauseAction.performed += ctx => stateMachine.SwitchState("PAUSE");
+        }
+        private void ChangeToGameOverWhenPlayerDiesOrTimeEnds(StateMachine stateMachine)
+        {
+            if(_playerHealth.HasDied || _gameTimer.HasTimerReachedZero)
+            {
+                stateMachine.SwitchState("GAMEOVER");
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameManager/StateMachine/States/PauseState.cs
@@ -0,0 +1,39 @@
+﻿namespace Assets.Scripts.GameManager.StateMachine
+{
+    using Devic.Scripts.Utils.StateMachine;
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+
+    public class PauseState : IConcreteState
+    {
+        private PlayerInput _inputSystem;
+        private InputAction _pauseAction;
+
+        public PauseState(PlayerInput inputSystem)
+        {
+            _inputSystem = inputSystem;
+            _pauseAction = _inputSystem.actions.FindAction("Pause");
+        }
+
+        public void OnStateEnter(StateMachine stateMachine)
+        {
+            PauseGame();
+        }
+        private void PauseGame()
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+        }
+        public void OnUpdateState(StateMachine stateMachine)
+        {
+            ChangeToPauseStateWhenPauseActionIsPerformed(stateMachine);
+
+        }
+        private void ChangeToPauseStateWhenPauseActionIsPerformed(StateMachine stateMachine)
+        {
+            _pauseAction.performed += ctx => stateMachine.SwitchState("GAMEPLAY");
+        }
+    }
+}
