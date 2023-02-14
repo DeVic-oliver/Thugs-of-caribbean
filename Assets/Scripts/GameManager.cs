57119cb5fd020e23d3b570da03a7ee97320128ef
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-14 11:58:51 -0300

    game manager implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Core.Components.Counters;
+using Assets.Scripts.Player;
+[RequireComponent(typeof(TimerCounter))]
+public class GameManager : MonoBehaviour
+{
+    private TimerCounter _timer;
+    [SerializeField] private PlayerHealth _playerHealth;
+    [SerializeField] private GameObject _pauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = GetComponent<TimerCounter>();
+        _timer.StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
