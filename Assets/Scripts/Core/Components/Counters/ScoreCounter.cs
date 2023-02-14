0d492b2b92a28be347cc816bf4dff52252b5a573
Victor Matheus <victormatheus183@gmail.com>
2023-02-13 22:37:45 -0300

    Scorecounter component implemented

---
+++ Assets/Scripts/Core/Components/Counters/ScoreCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Components.Counters
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        public static int PlayerPoints;
+
+        [SerializeField] private TextMeshProUGUI _pointCounterUI;
+
+
+        private void LateUpdate()
+        {
+            _pointCounterUI.text = $"Score: {PlayerPoints.ToString()}";
+        }
+    }
+}
