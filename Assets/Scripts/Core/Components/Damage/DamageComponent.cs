0da1b4ddff1a3a618c141889f35e31eed45a4f63
Victor Matheus <victormatheus183@gmail.com>
2023-02-13 22:38:57 -0300

    Damage Component simplified
    Plays a death VFX with sprites iteration for explosion effect

--- Assets/Scripts/Core/Components/Damage/DamageComponent.cs
+++ Assets/Scripts/Core/Components/Damage/DamageComponent.cs
@@ -1,6 +1,7 @@
 ﻿using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts.Core.Components.Damage
@@ -11,9 +12,13 @@ namespace Assets.Scripts.Core.Components.Damage
         [SerializeField] private SpriteRenderer _renderer;
         [SerializeField] private Color _damageFeedbackColor;
         [SerializeField] private float _flashSpeed = 1f;
+        [SerializeField] private ParticleSystem _deathVFX;
 
-        public List<VFXDict> _vfxDict = new List<VFXDict>();
-        private Dictionary<string, ParticleSystem> dict = new();
+        [Header("Explosion Sprite Settings")]
+        [SerializeField] private SpriteRenderer _explosionGameObject;
+        [SerializeField] private List<Sprite> _explosionSpriteList;
+        private Coroutine _explosionCoroutine;
+        private float _spriteChangeSpeed = 0.1f;
 
         private Tween _currentColorTween;
 
@@ -25,33 +30,35 @@ namespace Assets.Scripts.Core.Components.Damage
             }
         }
 
-        public ParticleSystem GetVFXFromDict(string id)
+        public void PlayDeathVFX()
         {
-            if (dict.ContainsKey(id))
-            {
-                return dict[id];
-            }
-
-            return null;
+            _deathVFX.Play();
+            PlaySpriteExplistion();
         }
 
-        private void Start()
+        private void PlaySpriteExplistion() 
         {
-            PopulateVFXDictionary();
-        }
-        private void PopulateVFXDictionary()
-        {
-            foreach (var item in _vfxDict)
+            if(_explosionCoroutine == null)
             {
-                dict.Add(item.ID, item.VFX);
+                StartCoroutine("ChangeExplosionSpritesFowardsAndBackwards");
             }
         }
-    }
+        private IEnumerator ChangeExplosionSpritesFowardsAndBackwards()
+        {
+            foreach (var sprite in _explosionSpriteList)
+            {
+                _explosionGameObject.sprite = sprite;
+                yield return new WaitForSeconds(_spriteChangeSpeed);
+            }
 
-    [System.Serializable]
-    public class VFXDict
-    {
-        public string ID;
-        public ParticleSystem VFX;
+            for (int spriteIndex = _explosionSpriteList.Count - 1; spriteIndex > 0; spriteIndex--)
+            {
+                _explosionGameObject.sprite = _explosionSpriteList[spriteIndex];
+                yield return new WaitForSeconds(_spriteChangeSpeed);
+            }
+            _explosionGameObject.sprite = null;
+            _explosionCoroutine = null;
+        }
+
     }
 }
