e72038fcfa28d211608287fb875664867f5e56be
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-15 16:18:46 -0300

    spawner first implementantion

---
+++ Assets/Scripts/Core/Components/Spawner/SpawnArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Core.Components.Spawner
+{
+    public class SpawnArea : MonoBehaviour
+    {
+        public bool IsInvisible { get; private set; }
+
+        private void OnBecameInvisible()
+        {
+            IsInvisible = true;
+        }
+
+        private void OnBecameVisible()
+        {
+            IsInvisible = false;
+        }
+
+        public void SpawnGameObject(GameObject gameObject)
+        {
+            Debug.Log("SPAWNED AT:" + transform.position);
+            var obj = Instantiate(gameObject, gameObject.transform);
+            obj.transform.SetParent(null);
+        }
+    }
+}
