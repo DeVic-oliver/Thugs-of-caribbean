33260c2ff74f72bd3826cb3edae17be94cb09c16
Victor Matheus <victormatheus183@gmail.com>
2023-02-16 11:48:25 -0300

    directives not used

--- Assets/Scripts/Core/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Core/Enemies/EnemyHealth.cs
@@ -1,8 +1,4 @@
 using Assets.Scripts.Core.Components;
-using Assets.Scripts.Core.Components.Counters;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace Assets.Scripts.Core.Enemies { 
     public class EnemyHealth : Health
