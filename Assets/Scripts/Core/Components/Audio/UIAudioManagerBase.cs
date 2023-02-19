67d96646e3f410aca5ec0e4940e9056591487d8f
Victor Matheus <victormatheus183@gmail.com>
2023-02-18 21:46:47 -0300

    UI Audio manager created

---
+++ Assets/Scripts/Core/Components/Audio/UIAudioManagerBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Core.Components.Audio
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(AudioSource))]
+    public class UIAudioManagerBase : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        [SerializeField] private AudioClip _buttonClick;
+        [SerializeField] private AudioClip _closeSound;
+
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayButtonClick()
+        {
+            _audioSource.PlayOneShot(_buttonClick);
+        }
+
+        public void PlayCloseSound()
+        {
+            _audioSource.PlayOneShot(_closeSound);
+        }
+
+    }
+}
