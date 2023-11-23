ad6a10ce536b655e59cd0f94625215d76fa08b3e
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 20:54:31 -0300

    ChaserHealth class created
    Inherits from base Health class.
    Chaser auto destroys itself when collide with player, so it makes more sense that only him have a method that set the health to zero

---
+++ Assets/Scripts/Enemies/Ship/Minion/Chaser/ChaserHealth.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Enemies.Ship.Minion.Chaser
+{
+    using Assets.Scripts.Core.Components;
+
+    public class ChaserHealth : Health
+    {
+        public void LoseAllHealthPoints()
+        {
+            DecreaseHealth(CurrentHealth);
+        }
+    }
+}
