7db527e6c937c17b71ead1f63168cf8ae1e85ec9
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 15:35:08 -0300

    Minor changes

--- Assets/Scripts/Player/Guns/PlayerGun.cs
+++ Assets/Scripts/Player/Guns/PlayerGun.cs
@@ -20,7 +20,7 @@ public class PlayerGun : MonoBehaviour
     
     private GunBase _currentGun;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         PopulatePlayerGuns();
--- Assets/Scripts/Weapons/Guns/Pistol.cs
+++ Assets/Scripts/Weapons/Guns/Pistol.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections;
-using UnityEngine;
-
-namespace Assets.Scripts.Weapons.Guns
+﻿namespace Assets.Scripts.Weapons.Guns
 {
     public class Pistol : GunBase
     {
-
-        // Update is called once per frame
         protected override void Update()
         {
            base.Update();
