f1c1121163eb7112821f3eaf0caaacf0e71c67db
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 15:34:58 -0300

    Chaser detonation component created

---
+++ Assets/Scripts/Enemies/Ship/Minion/Chaser/ChaserDetonation.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Enemies.Ship.Minion.Chaser
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class ChaserDetonation : MonoBehaviour
+    {
+        public UnityEvent OnDetonation;
+
+        private readonly string _detonationTag = "Player";
+
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.CompareTag(_detonationTag))
+            {
+                OnDetonation?.Invoke();
+            }
+        }
+    }
+}
