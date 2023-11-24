c2e1568a5e106c8cd409c4afc8fabf6c960742df
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 23:25:39 -0300

    Inheriting common movement behavior's

---
+++ Assets/Scripts/Enemies/Ship/Minion/EnemyShipMovement.cs
@@ -0,0 +1,41 @@
+﻿namespace Assets.Scripts.Enemies.Ship.Minion
+{
+    using Assets.Scripts.Core.Enemies;
+    using UnityEngine;
+
+    [RequireComponent(typeof(EnemyShipRotation))]
+    public class EnemyShipMovement : MonoBehaviour
+    {
+        [SerializeField] protected GameObject _objectToMovement;
+        [SerializeField] protected TargetNearbyDetector _detector;
+        [SerializeField] protected EnemyShipRotation _shipRotation;
+
+        protected bool _shouldPursueTarget;
+
+
+        public void PreventPursue()
+        {
+            _shouldPursueTarget = false;
+        }
+
+        public void AllowPursue()
+        {
+            _shouldPursueTarget = true;
+        }
+
+        protected Transform GetObjectTransform()
+        {
+            return _objectToMovement.transform;
+        }
+
+        protected Transform GetTargetTransform()
+        {
+            return _detector.Target.transform;
+        }
+
+        protected void Awake()
+        {
+            _shipRotation = GetComponent<EnemyShipRotation>();
+        }
+    }
+}
