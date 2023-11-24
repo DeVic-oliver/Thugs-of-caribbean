3dbe16d15ab1a5507316e86e07b7e9447eb4c1ea
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 23:52:04 -0300

    ShooterAttack class implemented
    Handles with the shooter enemy type attack when player is within the detector range and when its on the enemy sight

---
+++ Assets/Scripts/Enemies/Ship/Minion/Shooter/ShooterAttack.cs
@@ -0,0 +1,38 @@
+﻿namespace Assets.Scripts.Enemies.Ship.Minion.Shooter
+{
+    using Assets.Scripts.Core.Components.Weapon;
+    using Assets.Scripts.Core.Enemies;
+    using Assets.Scripts.Utils.SightRaycast._2D;
+    using UnityEngine;
+    
+
+    public class ShooterAttack : MonoBehaviour
+    {
+        [SerializeField] private RangedWeapon _weapon;
+        [SerializeField] private bool _isEnemyOnSight;
+        [SerializeField] private TargetNearbyDetector _targetNearbyDetector;
+
+        private readonly string _targetLayerMask = "Player";
+
+        private bool _canAttack;
+
+
+        public void AllowAttack()
+        {
+            _canAttack = true;
+        }
+
+        public void PreventAttack() 
+        {
+            _canAttack = false;
+        }
+
+        private void Update()
+        {
+            _isEnemyOnSight = SightRaycaster2D.CheckGameObjectOnSight(transform, _targetNearbyDetector.GetRangeDetection(), _targetLayerMask);
+
+            if (_canAttack && _isEnemyOnSight)
+                _weapon.Shoot();
+        }
+    }
+}
