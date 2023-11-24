0ee66003a8156b6f618f1d459d390ceaeb0658b9
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 22:18:05 -0300

    A detector to detect the distance to target
    Fire events when target is close and when it is not close

---
+++ Assets/Scripts/Core/Enemies/TargetNearbyDetector.cs
@@ -0,0 +1,39 @@
+﻿namespace Assets.Scripts.Core.Enemies
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class TargetNearbyDetector : MonoBehaviour
+    {
+        public UnityEvent OnTargetNearby;
+        public UnityEvent OnTargetNotNearby;
+        public GameObject Target;
+        
+        [SerializeField] private float _rangeDetection = 15; 
+
+
+        private void Update()
+        {
+            if (IsTargetNearby())
+            {
+                OnTargetNearby?.Invoke();
+            }
+            else
+            {
+                OnTargetNotNearby?.Invoke();
+            }
+        }
+
+        private bool IsTargetNearby()
+        {
+            float distance = Vector3.Distance(transform.position, GetTargetTransform().position);
+            return distance < _rangeDetection;
+        }
+        
+        private Transform GetTargetTransform()
+        {
+            return Target.transform;
+        }
+
+    }
+}
