44d11716e23912bab91316c2a5ac62729a361f3d
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 22:45:25 -0300

    A simple gateway that passes the damage to a health

---
+++ Assets/Scripts/Core/Components/Damage/DamageGateway.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Scripts.Core.Components.Damage
+{
+    using UnityEngine;
+
+    public class DamageGateway : MonoBehaviour
+    {
+        public Health _health;
+
+
+        public void ApplyDamageOnHealth(float damage)
+        {
+            _health.DecreaseHealth(damage);
+        }
+    }
+}
