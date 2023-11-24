628a527a7ac8eec24aa5397d03583fbae6db4362
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-23 22:46:33 -0300

    Chaser damage detonation
    When hits the player, it gets the player damage gateway and passes the damage trough param

--- Assets/Scripts/Enemies/Ship/Minion/Chaser/ChaserDetonation.cs
+++ Assets/Scripts/Enemies/Ship/Minion/Chaser/ChaserDetonation.cs
@@ -1,11 +1,14 @@
 ﻿namespace Assets.Scripts.Enemies.Ship.Minion.Chaser
 {
+    using Assets.Scripts.Core.Components.Damage;
     using UnityEngine;
     using UnityEngine.Events;
 
     public class ChaserDetonation : MonoBehaviour
     {
         public UnityEvent OnDetonation;
+
+        [SerializeField] private float _damage = 25f;
 
         private readonly string _detonationTag = "Player";
 
@@ -15,6 +18,8 @@
             if(collision.gameObject.CompareTag(_detonationTag))
             {
                 OnDetonation?.Invoke();
+                DamageGateway damageGateway = collision.gameObject.GetComponent<DamageGateway>();
+                damageGateway.ApplyDamageOnHealth( _damage );
             }
         }
     }
