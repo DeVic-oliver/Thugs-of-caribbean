35fb51b81a61f1451bd1e72391b559fdb54e68b2
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-25 13:17:46 -0300

    Duplicate code deleted and inserted into ExplosionSpriteChanger

--- Assets/Scripts/Core/Components/2DComponents/ExplosionSpriteChanger.cs
+++ Assets/Scripts/Core/Components/2DComponents/ExplosionSpriteChanger.cs
@@ -1,1 +1,60 @@
+﻿namespace Assets.Scripts.Core.Components._2DComponents
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    
     public class ExplosionSpriteChanger : MonoBehaviour
+    {
+        [Header("Explosion Sprite Settings")]
+        [SerializeField] private SpriteRenderer _explosionGameObject;
+        [SerializeField] private List<Sprite> _explosionSpriteList;
+        
+        private Coroutine _explosionCoroutine;
+        private float _spriteChangeSpeed = 0.1f;
+
+
+        public void PlaySpriteExplosion()
+        {
+            if (_explosionCoroutine == null)
+                StartCoroutine(nameof(ChangeExplosionSpritesFowardsAndBackwards));
+        }
+
+        private IEnumerator ChangeExplosionSpritesFowardsAndBackwards()
+        {
+            yield return ChangeSpritesForwards();
+            yield return ChangeSpriteBackwards();
+
+            _explosionGameObject.sprite = null;
+            _explosionCoroutine = null;
+        }
+
+        private IEnumerator ChangeSpritesForwards()
+        {
+            foreach (Sprite sprite in _explosionSpriteList)
+            {
+                ChangeExplostionSpriteTo(sprite);
+                yield return ReturnWaitForSeconds();
+            }
+        }
+
+        private IEnumerator ChangeSpriteBackwards()
+        {
+            for (int spriteIndex = _explosionSpriteList.Count - 1; spriteIndex > 0; spriteIndex--)
+            {
+                ChangeExplostionSpriteTo(_explosionSpriteList[spriteIndex]);
+                yield return ReturnWaitForSeconds();
+            }
+        }
+
+        private void ChangeExplostionSpriteTo(Sprite newSprite)
+        {
+            _explosionGameObject.sprite = newSprite;
+        }
+
+        private IEnumerator ReturnWaitForSeconds()
+        {
+            yield return new WaitForSeconds(_spriteChangeSpeed);
+        }
+    }
+}
--- Assets/Scripts/Core/Components/Damage/VisualDamageFeedback.cs
+++ Assets/Scripts/Core/Components/Damage/VisualDamageFeedback.cs
@@ -1,11 +1,11 @@
 ﻿namespace Assets.Scripts.Core.Components.Damage
 {
+    using Assets.Scripts.Core.Components._2DComponents;
     using DG.Tweening;
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEditor;
     using UnityEngine;
-    
+
+    [RequireComponent(typeof(ExplosionSpriteChanger))]
     public class VisualDamageFeedback : MonoBehaviour
     {
         [Header("Damage settings")]
@@ -14,12 +14,7 @@
         [SerializeField] private float _flashSpeed = 1f;
         [SerializeField] private ParticleSystem _deathVFX;
 
-        [Header("Explosion Sprite Settings")]
-        [SerializeField] private SpriteRenderer _explosionGameObject;
-        [SerializeField] private List<Sprite> _explosionSpriteList;
-        [SerializeField] private float _spriteChangeSpeed = 0.1f;
-        
-        private Coroutine _explosionCoroutine;
+        private ExplosionSpriteChanger _explosionSpriteChanger;
         private Tween _currentColorTween;
 
 
@@ -32,50 +27,12 @@
         public void PlayDeathVFX()
         {
             _deathVFX.Play();
-            PlaySpriteExplosion();
+            _explosionSpriteChanger.PlaySpriteExplosion();
         }
 
-        private void PlaySpriteExplosion() 
+        private void Awake()
         {
-            if(_explosionCoroutine == null)
-                StartCoroutine(nameof(ChangeExplosionSpritesFowardsAndBackwards));
-        }
-
-        private IEnumerator ChangeExplosionSpritesFowardsAndBackwards()
-        {
-            yield return ChangeSpritesForwards();
-            yield return ChangeSpriteBackwards();
-
-            _explosionGameObject.sprite = null;
-            _explosionCoroutine = null;
-        }
-
-        private IEnumerator ChangeSpritesForwards()
-        {
-            foreach (Sprite sprite in _explosionSpriteList)
-            {
-                ChangeExplostionSpriteTo(sprite);
-                yield return ReturnWaitForSeconds();
-            }
-        }
-
-        private IEnumerator ChangeSpriteBackwards()
-        {
-            for (int spriteIndex = _explosionSpriteList.Count - 1; spriteIndex > 0; spriteIndex--)
-            {
-                ChangeExplostionSpriteTo(_explosionSpriteList[spriteIndex]);
-                yield return ReturnWaitForSeconds();
-            }
-        }
-
-        private void ChangeExplostionSpriteTo(Sprite newSprite)
-        {
-            _explosionGameObject.sprite = newSprite;
-        }
-
-        private IEnumerator ReturnWaitForSeconds()
-        {
-            yield return new WaitForSeconds(_spriteChangeSpeed);
+            _explosionSpriteChanger = GetComponent<ExplosionSpriteChanger>();
         }
 
         private void Start()
