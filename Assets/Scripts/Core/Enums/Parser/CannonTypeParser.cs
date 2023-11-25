51b25a9a56ec43b11f482ed8c5fc0ff9e3c09ed1
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-25 15:35:36 -0300

    Simple parser for CannonType enum

---
+++ Assets/Scripts/Core/Enums/Parser/CannonTypeParser.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.Core.Enums.Parser
+{
+    public static class CannonTypeParser
+    {
+           
+        public static bool IsSingleCannon(CannonTypes type)
+        {
+            return (CannonTypes.Single == type);
+        }
+
+        public static bool IsMultipleCannon(CannonTypes type)
+        {
+            return (CannonTypes.Multiple == type);
+        }
+
+    }
+}
