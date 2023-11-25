b8f5128f72c4d9f713ab222e75d30eae3a0e098f
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-25 13:45:55 -0300

    ScoreCounter now only increments or reset the count

--- Assets/Scripts/Core/Components/Counters/ScoreCounter.cs
+++ Assets/Scripts/Core/Components/Counters/ScoreCounter.cs
@@ -7,16 +7,23 @@ namespace Assets.Scripts.Core.Components.Counters
 {
     public class ScoreCounter : MonoBehaviour
     {
-        public static int PlayerPoints;
+        public static int Score { get; private set; }
 
         [SerializeField] private List<TextMeshProUGUI> _pointCounterUI;
 
         private void LateUpdate()
+        public static void ResetScore()
+        {
+            Score = 0;
+        }
+
+        public static void IncrementScore()
         {
             foreach (var counter in _pointCounterUI)
             {
                 counter.text = $"Score: {PlayerPoints.ToString()}";
             }
+            Score++;
         }
 
         public void ResetScore()
