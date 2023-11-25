e18ef0eacb60727bd2cfc8483ac44a19e516cfd1
Victor Matheus <matheuslima.o183@gmail.com>
2023-11-25 12:47:28 -0300

    Disabled refactored to destroyer

---
+++ Assets/Scripts/Core/Components/Utils/GameObjectDelayedDestroy.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Core.Components.Utils
+{
+    using System.Collections;
+    using UnityEngine;
+    
+    public class GameObjectDelayedDestroyer : MonoBehaviour
+    {
+        [SerializeField] protected float _secondsToDestroy = 1.5f;
+
+        public void DestroyGameObjectAfterDelay()
+        {
+            StartCoroutine(nameof(DestroyInstance));
+        }
+
+        private IEnumerator DestroyInstance()
+        {
+            yield return new WaitForSeconds(_secondsToDestroy);
+            Destroy(gameObject);
+        }
+    }
+}
