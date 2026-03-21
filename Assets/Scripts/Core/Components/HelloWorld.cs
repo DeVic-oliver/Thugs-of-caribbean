934e4d09c301ce9488788cd7fd40a41e359d7b5f
Victor Matheus <matheuslima.o183@gmail.com>
2026-03-21 16:44:13 -0300

    Hello World

---
+++ Assets/Scripts/Core/Components/HelloWorld.cs
@@ -0,0 +1,12 @@
+namespace Assets.Scripts.Core.Components
+{
+    using UnityEngine;
+
+    public class HelloWorld : MonoBehaviour
+    {
+        private void Update()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
